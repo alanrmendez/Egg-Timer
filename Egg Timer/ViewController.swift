8d19f164d73f0507edac8f35932a15907d08b17c
alanrmendez <alan.mendez07@gmail.com>
2021-01-22 23:52:55 -0500

    Cleaned the code, removing extraneous lines and redundant '"if let"/"if var" structures.

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -14,64 +14,51 @@ class ViewController: UIViewController {
     //timer object
     var timer = Timer()
     
-    
+    var eggTimer = 210
     
     @IBOutlet weak var timeLabel: UILabel!
     
     
     //function associated with timer object
     @objc func processTimer() {
-        if let eggTimerString = timeLabel.text {
-            let eggTimerInt = Int(eggTimerString)
-            if var eggTimer = eggTimerInt {
-                if eggTimer <= 0 {
-                    timer.invalidate()
-                    eggTimer = 0
-                } else {eggTimer -= 1}
-                timeLabel.text = String(eggTimer)
-            }
-            else {print("error: value not a number")}
+        if eggTimer > 0 {
+            eggTimer -= 1
+            timeLabel.text = String(eggTimer)
         }
-        else {print("error: value not a number")}
+            else {
+                timer.invalidate()
+            }
     }
 
     
     @IBAction func pausePressed(_ sender: Any) {
         timer.invalidate()
     }
-    
+     
     
     @IBAction func playPressed(_ sender: Any) {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(processTimer), userInfo: nil, repeats: true)
     }
     
     @IBAction func minusTenPressed(_ sender: Any) {
-        if let eggTimerString = timeLabel.text {
-            let eggTimerInt = Int(eggTimerString)
-            if var eggTimer = eggTimerInt {
-                if eggTimer - 10 <= 0 {
-                    timer.invalidate()
-                    eggTimer = 0
-                } else {eggTimer -= 10}
-                timeLabel.text = String(eggTimer)
-            }
+        if eggTimer >= 10 {
+            eggTimer -= 10
+            timeLabel.text = String(eggTimer)
         }
     }
     
     @IBAction func plusTenPressed(_ sender: Any) {
-        if let eggTimerString = timeLabel.text {
-            let eggTimerInt = Int(eggTimerString)
-            if var eggTimer = eggTimerInt {
-                eggTimer += 10
-                timeLabel.text = String(eggTimer)
-            }
-        }
+        eggTimer += 10
+        timeLabel.text = String(eggTimer)
     }
+        
     
     @IBAction func resetPressed(_ sender: Any) {
         timer.invalidate()
         timeLabel.text = "210"
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
